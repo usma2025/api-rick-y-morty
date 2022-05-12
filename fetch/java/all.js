b12f90f5ec4f6bdbfd4bb56a04fc54a1bb1ac594
usma2025 <73004488+usma2025@users.noreply.github.com>
2022-05-12 18:23:30 -0500

    Add files via upload

---
+++ fetch/java/all.js
@@ -0,0 +1,86 @@
+//All
+function consumeData(){
+    fetch('https://rickandmortyapi.com/api/character')
+    .then((response) => response.json())
+    .then((data) => {
+        let html=''
+        let i = 0
+        let icon = ''
+        let estado = ''
+        
+        data.results.map((character) =>{
+               
+            //genero(character.gender)
+            if(character.gender == "Male" ){
+                icon='<i class="fa-solid fa-mars"></i>'
+            }
+            else if(character.gender == "Female"){
+                icon='<i class="fa-solid fa-venus"></i>'
+            }   
+            
+            if(character.status == "Alive"){
+                estado='green'
+            }
+            else if(character.status =="Dead"){
+                estado='red'
+            }
+            else if(character.status =="unknown"){
+                estado='gray'
+            }
+            
+            
+
+            i= character.episode.length
+            html += `
+            
+            <article key="${character.name}" class="listCharacters--character">
+            <div class="listCharacters--character-container">
+                <img src="${character.image}" alt"${character.name}"/>
+                <div class="listCharacters--character-container-2">
+                    <i class="fa-solid fa-circle" id="${estado}"></i>                        
+                    <h1>${character.name}</h1>
+                    <p><i class="fa-solid fa-location-pin"></i> Ubicacion: ${character.location.name}</p>
+                    <p> Apariciones: ${i}</p>
+                    ${icon} 
+                    
+                    
+                </div>
+            </div>                   
+            </article>`
+            
+        })
+        
+        document .getElementById('listCharacters').innerHTML = html
+    })
+    .catch((err)=>{
+        console.info(err)
+    })
+    
+
+}
+function contador(){
+    data.results.map((character) =>{
+        if(character.gender=="Male"){
+            hombre=hombre+1
+        }
+        else if(character.gender=="Female"){
+            mujer=mujer+1
+        }
+        
+    })
+}
+
+
+//Death
+
+
+
+// function genero(gender){
+//     if(gender == "Male" ){
+//         icon='<i class="fa-solid fa-mars"></i>'
+//     }
+//     else if(gender == "Female"){
+//         icon='<i class="fa-solid fa-venus"></i>'
+//     }
+//     return gender
+// }
---
+++ fetch/java/death.js
@@ -0,0 +1,54 @@
+//Death
+function consumeData3(){
+    fetch('https://rickandmortyapi.com/api/character')
+    .then((response) => response.json())
+    .then((data) => {
+        let html=''
+        let i = 0
+        let icon = ''
+        let estado = ''
+        data.results.map((character) =>{
+            if(character.status=="Dead"){  
+            //genero(character.gender)
+            if(character.gender == "Male" ){
+                icon='<i class="fa-solid fa-mars"></i>'
+            }
+            else if(character.gender == "Female"){
+                icon='<i class="fa-solid fa-venus"></i>'
+            }   
+            
+            if(character.status == "Alive"){
+                estado='green'
+            }
+            else if(character.status =="Dead"){
+                estado='red'
+            }
+            else if(character.status =="unknown"){
+                estado='gray'
+            }
+            
+
+            i= character.episode.length
+            html += `<article key="${character.name}" class="listCharacters--character">
+            <div class="listCharacters--character-container">
+                <img src="${character.image}" alt"${character.name}"/>
+                <div class="listCharacters--character-container-2">
+                    <i class="fa-solid fa-circle" id="${estado}"></i>                        
+                    <h1>${character.name}</h1>
+                    <p><i class="fa-solid fa-location-pin"></i> Ubicacion: ${character.location.name}</p>
+                    <p> Apariciones: ${i}</p>
+                    ${icon} 
+                    
+                </div>
+            </div>                   
+            </article>`
+        }
+        })
+        
+        document .getElementById('listCharacters').innerHTML = html
+    })
+    .catch((err)=>{
+        console.info(err)
+    })
+    
+}
